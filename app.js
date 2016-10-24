f8ca26a19d6853e797e8a1e0ceb69ed193e8836f
axel <axel.bouillart@gmail.com>
2016-10-24 15:52:15 +0200

    ajout var element

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-$(function(data) {
-  $('button').click(function(){
-    $.ajax('http://192.168.1.21:3000/places',function(data){
-      $('span').html(data);
-      })
-      .done(function() {
-        alert( 'success' );
-      })
-      .fail(function() {
-        alert( 'error' );
-      })
-      .always(function() {
-        alert( 'complete' );
-      });
-    });
-  });
+(function(){
+
+  $("button").click(function(){
+    var url = 'http://192.168.1.21:3000/places';
+
+    $.ajax(url).done(ajaxDone).fail(ajaxFail); 
+
+    $.ajaxDone(function(response){
+      var place = response.places;
+      console.log(places);
+      for(var i = 0; i<result.places.length; i++){
+        var place = place.places[i];
+        console.log(place);
+        var newElement = "<li>" + place.nom +"</li";
+        
+};
+})();
+//       $('span').html(result)
+//       if(place.nom === IoT Valley){
+//         var password = place.password;
+
+
+}
+//       }
+//     }
+
+//   })    
+
+// })
+// .fail(function() {
+//        // alert("error");
+//      })
+// .always(function() {
+//         //alert("complete");
+
+// Attention ça ne marche pas pour le moment !!!!!!
